b50cc48fd8dfc01e30f2da5908363dcf7973ec51
s28961-pj <s28961@pjwstk.edu.pl>
2024-06-29 17:42:49 +0200

    initial commit with Spring Initializr

---
+++ src/main/java/me/rolandliedtke/jaz_s28961_nbp/JazS28961NbpApplication.java
@@ -0,0 +1,13 @@
+package me.rolandliedtke.jaz_s28961_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS28961NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS28961NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/rolandliedtke/jaz_s28961_nbp/JazS28961NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rolandliedtke.jaz_s28961_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS28961NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
